c08931d3aacd4dba3664c997a66111de763b8f0f
jbenfield108@gmail.com <jbenfield108@gmail.com>
2021-07-11 08:33:52 -0400

    Deactivate role

--- Lib/MainDb.Entities/AppRoleRecord.cs
+++ Lib/MainDb.Entities/AppRoleRecord.cs
@@ -1,9 +1,12 @@
-﻿namespace MainDB.Entities
+﻿using System;
+
+namespace MainDB.Entities
 {
     public sealed class AppRoleRecord
     {
         public int ID { get; set; }
         public int AppID { get; set; }
         public string Name { get; set; } = "xti_notfound";
+        public DateTimeOffset TimeDeactivated { get; set; } = DateTimeOffset.MaxValue;
     }
 }
